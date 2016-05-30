e3f44d399cb02a48dd99653978b4b78d6ad7cad2
Nesiehr <rheisen@cataclysm.io>
2016-05-30 10:51:01 -0400

    foundation grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 // 	render: function() {
@@ -10,7 +11,18 @@ var React = require('react');
 
 var Examples = (props) => {
 	return (
-		<h3>Examples</h3>
+		<section>
+			<h1 className='text-center'>Examples</h1>
+			<p> Here are a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to='/?location=New%20York'>New York, NY</Link>
+				</li>
+				<li>
+					<Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+				</li>
+			</ol>
+		</section>
 	)
 };
 
