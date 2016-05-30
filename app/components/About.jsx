d55e26a1daea9024f80f54a4a63e98d16f808b15
Nesiehr <rheisen@cataclysm.io>
2016-05-30 10:59:46 -0400

    about page resource links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,8 @@ var About = (props) => {
 	return(
 		<section>
 			<h1 className='text-center'>About</h1>
-			<p>Welcome to React Weather. This web application uses React and OpenWeatherMap to display the temperature in different cities.</p>
+			<p>Welcome to React Weather. This web application uses <a href='https://facebook.github.io/react' target='_blank'>React</a> and <a href='https://openweathermap.org' target='_blank'>OpenWeatherMap</a> to display the temperature in different cities.</p>
+			<a href='https://github.com/Nesiehr/react-weather' target="_blank">Link to the github repo</a>
 		</section>
 	)
 };
